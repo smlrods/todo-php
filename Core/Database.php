f60779290a03c5a98859aff74e3c5ff5ba505835
smlrods <smlrodrigues.dev@gmail.com>
2023-08-18 18:00:11 -0300

    Add missing colon in `Database.php`

--- Core/Database.php
+++ Core/Database.php
@@ -11,7 +11,7 @@ class Database
 
   public function __construct($config, $username = 'root', $password = '')
   {
-    $dsn = 'mysql' . http_build_query($config, '', ';');
+    $dsn = 'mysql:' . http_build_query($config, '', ';');
 
     $this->connection = new PDO($dsn, $username, $password, [
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
