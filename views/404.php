a87aa52ec6d3c72583d6f937265942b5de79b354
smlrods <smlrodrigues.dev@gmail.com>
2023-08-19 19:48:06 -0300

    Implement 404 view

---
+++ views/404.php
@@ -0,0 +1,16 @@
+<?php require('partials/head.php') ?>
+
+<main>
+    <div>
+        <h1>Sorry. Page Not Found.</h1>
+        <div>
+          <img src="https://gifdb.com/images/high/killua-cute-confused-face-kltxtdwwx2in6j05.gif" />
+        </div>
+
+        <p>
+            <a href="/">Go back home.</a>
+        </p>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
