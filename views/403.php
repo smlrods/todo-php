8f1567ff8ade9196ba86f5b6c6fcb4aee01b79dc
smlrods <smlrodrigues.dev@gmail.com>
2023-08-19 19:47:56 -0300

    Implement 403 view

---
+++ views/403.php
@@ -0,0 +1,15 @@
+<?php require('partials/head.php') ?>
+
+<main>
+    <div>
+        <h1>You are not authorized to view this page.</h1>
+        <div>
+          <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8d8aa80d-a00f-4ca5-817d-30fbc6bf33be/da1eeb7-52b3cd75-2fef-467d-9ccd-d8b19ec8740d.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhkOGFhODBkLWEwMGYtNGNhNS04MTdkLTMwZmJjNmJmMzNiZVwvZGExZWViNy01MmIzY2Q3NS0yZmVmLTQ2N2QtOWNjZC1kOGIxOWVjODc0MGQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.zQULJzn0WtiHilwDuWr6HLdaf3BMzykkKxyn4AggTRk" />
+        </div>
+        <p class="mt-4">
+            <a href="/">Go back home.</a>
+        </p>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
